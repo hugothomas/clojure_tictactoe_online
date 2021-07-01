120a90187bde71792ec3518e1cb4f56dcf1a5ad5
hthomas <hugo.thomas98@gmail.com>
2021-07-01 15:19:57 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject tictictoe_online "0.1.0-SNAPSHOT"
   :description "A school exercise, tic tac toe webapp."
   :url "https://github.com/hugothomas/clojure_tictactoe_online.git"
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"],[ring/ring-core "1.9.3"],[hiccup "1.0.5"],[compojure "1.6.2"]]
   :main ^:skip-aot tictictoe-online.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
