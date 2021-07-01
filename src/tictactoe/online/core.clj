5c16e057b29b645d8cbb82d2f9548240d29fe893
Kuinox <nvandeginste@gmail.com>
2021-07-01 17:05:40 +0200

    wip

--- src/tictactoe/online/core.clj
+++ src/tictactoe/online/core.clj
@@ -2,9 +2,13 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
-
+(use [tictactoe.online.boardUtils])
 (defroutes app-routes
-   (GET "/" [] ( :h1 "Hello World"))
+   (GET "/" [] ( :h1 (displayBoard [
+                      "x" "o" "x"
+                      "o" "x" "."
+                      "x" "o" "."
+                      ] (:div "x") (:div "o") (:div "ntm hugo") "" )))
    (route/not-found (:h1 "Page not found")))
 (def app
   (wrap-defaults app-routes site-defaults))
