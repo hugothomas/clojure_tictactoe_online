33d963290b05e7f6446f6295df7f8c56a52645c0
Kuinox <nvandeginste@gmail.com>
2021-07-01 17:11:57 +0200

    SHIP IT (2)

--- src/tictactoe/online/core.clj
+++ src/tictactoe/online/core.clj
@@ -1,10 +1,11 @@
 (ns tictactoe.online.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [hiccup.core :refer :all]))
 
 (defroutes app-routes
-   (GET "/" [] ( :h1 "Hello World"))
-   (route/not-found (:h1 "Page not found")))
+   (GET "/" []  (html [:h1 "Hello World"]))
+   (route/not-found (html [:h1 "Page not found"])))
 (def app
   (wrap-defaults app-routes site-defaults))
