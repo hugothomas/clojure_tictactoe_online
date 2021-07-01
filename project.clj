26df43ecc6f22215f1db56f27968fe4d14f04c86
hthomas <hugo.thomas98@gmail.com>
2021-07-01 15:33:05 +0200

    tar profile

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"],[ring/ring-core "1.9.3"],[hiccup "1.0.5"],[compojure "1.6.2"]]
   :main ^:skip-aot tictictoe-online.core
   :target-path "target/%s"
+  :plugins [[lein-tar "3.2.0"]]
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
