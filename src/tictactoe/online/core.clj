8f1883bda8e5473ffc5a46f0f268af815679e165
MaxElPadre <clairson.max@gmail.com>
2021-07-01 18:16:35 +0200

    fucking sheetstyle

--- src/tictactoe/online/core.clj
+++ src/tictactoe/online/core.clj
@@ -5,10 +5,7 @@
             [hiccup.core :refer :all]
             [tictactoe.online.boardUtils :refer :all]
             [ring.middleware.resource :refer :all]
-            )
-  (:use [hiccup.page :only (html5 include-css include-js)])
-  )
-
+            [hiccup.page :refer :all]))
 
 (route/resources "/")
 
@@ -17,17 +14,34 @@
                          [:head
                           [:title "Tic Tac Toe"]
                           (include-css "css/styles.css")]
-                          [:body
+                         [:body
 
-                            [:div {:class "ttt-board"} (displayBoard [
-                                                                "x" "o" "x"
-                                                                "o" "x" "."
-                                                                "x" "o" "."
-                                                                ] [:div "x"] [:div "o"] [:div "ntm hugo"])]
+                          [:div {:class "ttt-board"} (displayBoard [
+                                                                    "x" "o" "x"
+                                                                    "o" "x" "."
+                                                                    "x" "o" "."
+                                                                    ] [:div "x"] [:div "o"] [:div "ntm hugo"])]
 
-                           ]
-                          ))
-           (route/not-found (html [:h1 "Page not found"])))
+                          ]
+                         ))
+
+   (GET "/Board" [] (html [:head
+                           [:title "Board"]
+                           (include-css "css/board.css")]
+                          [:div.container
+                           [:h1" Tic-Tac-Toe "]
+                           [:div.play-area
+                            [:div#block_0.block]
+                            [:div#block_1.block]
+                            [:div#block_2.block]
+                            [:div#block_3.block]
+                            [:div#block_4.block]
+                            [:div#block_5.block]
+                            [:div#block_6.block]
+                            [:div#block_7.block]
+                            [:div#block_8.block]]]))
+
+   (route/not-found (html [:h1 "Page not found"])))
+
 (def app
-  (wrap-defaults app-routes site-defaults)
-  )
+  (wrap-defaults app-routes site-defaults))
